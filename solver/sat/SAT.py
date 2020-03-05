7bc5f5c7e197af3f0cce8ac08cb9093f668fe650
Oriol <oriolalascercos@gmail.com>
2020-03-05 10:34:30 +0100

    First commit of sat

---
+++ solver/sat/SAT.py
@@ -0,0 +1,12 @@
+from solver.data.formula import Formula
+
+
+class SAT(object):
+
+    def __init__(self, formula):
+        self.formula: Formula = formula
+        self.num_lits: int = formula.get_num_lits()
+
+    def get_random_interpretation(self):
+        num_lits = self.parser.get_vars()
+        raise NotImplementedError
