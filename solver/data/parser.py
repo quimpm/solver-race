ecff6d647178d8f3633a0c4e60bc408a5bedcb56
kimetix99 <qpico99@gmail.com>
2020-03-05 10:02:28 +0100

    Parser and Formula

--- solver/data/parser.py
+++ solver/data/parser.py
@@ -7,9 +7,7 @@ class Parser:
     def __init__(self, file_name):
         self.file_name = file_name
 
-    def get_formula():
-        literals = []
-        clause = []
+    def get_formula(self):
         formula = Formula()
         file = open(self.file_name)
         for line in file:
