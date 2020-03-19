6ea3d17c79f2a3d0d3b4e630c0b1612b4a199562
Oriol <oriolalascercos@gmail.com>
2020-03-19 11:01:46 +0100

    Created FracaSAT class (context class) It must be finished walk_sat function

--- fracaSAT.py
+++ fracaSAT.py
@@ -2,50 +2,83 @@
 import sys
 import random
 
-global formula
-global num_vars
-global not_finded
+
+class FracaSAT(object):
+
+    def __init__(self, formula, num_vars, not_found, num_clauses, clauses):
+        self.clauses = clauses
+        self.num_clauses = num_clauses
+        self.not_found = not_found
+        self.num_vars = num_vars
+        self.formula = formula
 
 
-def get_rnd_interpretation():
-    inter = [random.choice([-1, 1]) * (x + 1) for x in range(num_vars)]
-    for lit in not_finded:
+def get_rnd_interpretation(fracaSAT):
+    cost_clauses = [0 for _ in range(fracaSAT.num_clauses)]
+    inter = [random.choice([-1, 1]) * (x + 1) for x in range(fracaSAT.num_vars)]
+    for lit in fracaSAT.not_found:
         inter[abs(lit) - 1] = -lit
-    return inter
+    for lit in inter:
+        for c in fracaSAT.formula[lit]:
+            cost_clauses[c] += 1
+    return inter, cost_clauses
 
 
-def get_formula(file_name):
-    global formula
-    global num_vars
-    global not_finded
+def satisfies(cost_clauses):
+    return all(map(lambda x: x > 0, cost_clauses))
+
+
+def find_unsat_clause(cost_clauses):
+    return random.choice(list(filter(lambda x: x[1] == 0, enumerate(cost_clauses))))[0]
+
+
+def walk_sat(max_tries, max_flips, fracasat):
+    for i in range(max_tries):
+        inter, cost_clauses = get_rnd_interpretation(fracasat)
+        print(inter)
+        for j in range(max_flips):
+            if satisfies(cost_clauses):
+                return inter
+            c = find_unsat_clause(cost_clauses)
+            vars = fracasat.clauses[c]
+            print(c)
+            print(vars)
+
+
+def get_formula(file_name) -> FracaSAT:
     with open(file_name) as file:
         lines = file.readlines()
         for i, line in enumerate(lines):
             if line[0] == 'p':
-                num_vars = int(line.split()[2])
+                num_vars, num_clauses = map(int, line.split()[2:])
                 index_line = i
                 break
         formula = [[] for _ in range(num_vars + num_vars + 1)]
         for i, clause in enumerate(lines[index_line + 1:]):
             for lit in clause.split()[:-1]:
                 formula[int(lit)].append(i)
-        not_finded = []
+        clauses = [list(map(int, c.split()[:-1])) for c in lines[index_line + 1:]]
+        not_found = []
         for i, lit in enumerate(formula[1:num_vars + 1]):
             if not lit:
-                not_finded.append(i + 1)
+                not_found.append(i + 1)
         for i, lit in enumerate(formula[num_vars + 1:]):
             if not lit:
-                not_finded.append(-i - 1)
+                not_found.append(-i - 1)
+        return FracaSAT(formula, num_vars, not_found, num_clauses, clauses)
 
 
 def main():
     if len(sys.argv) != 2:
         sys.exit()
     else:
-        get_formula(sys.argv[1])
-    print(formula)
-    interpretation = get_rnd_interpretation()
+        fracaSAT = get_formula(sys.argv[1])
+    print(fracaSAT.formula)
+    interpretation, cost_clauses = get_rnd_interpretation(fracaSAT)
     print(interpretation)
+    print(cost_clauses)
+    print(satisfies(cost_clauses))
+    walk_sat(10, 10, fracaSAT)
 
 
 if __name__ == '__main__':
