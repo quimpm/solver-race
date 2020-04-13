911309bca6c7372cd0c672802e6f9c17966c6a6b
Oriol <oriolalascercos@gmail.com>
2020-04-13 17:44:18 +0200

    time_compare.sh with more tries

--- time_compare.sh
+++ time_compare.sh
@@ -3,6 +3,8 @@ echo 'TIME TABLES-------------------'
 TIMEFORMAT=%U
 for file in ./cnfs/*
 do
-    echo $file
-    echo $(time python3 fracaSAT.py $file | grep user) 
+    printf "\n$file\n"
+    for i in `seq 5`; do
+      echo -n $(time python3 fracaSAT.py $file | grep user)
+    done
 done
