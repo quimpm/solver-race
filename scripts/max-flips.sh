5ea36009089ff5ffda60893e0fc6e8e85b36091b
Oriol <oriolalascercos@gmail.com>
2020-04-21 17:33:20 +0200

    New max-flips.sh

--- scripts/max-flips.sh
+++ scripts/max-flips.sh
@@ -6,11 +6,11 @@ TIMEFORMAT=%U
 for file in ./benchmark-folder/*
 do
     printf "\n$file\n"
-    for j in 100 1000 10000; do
-      for i in 1 2 3 4 5 6 7 9; do
+    for j in 200; do
+      for i in {10..35}; do
         t=$(($i*$j))
         printf "\n$t\n"
-        for i in `seq 10`; do
+        for i in `seq 100`; do
           echo -n $(time python3 fracaSAT.py $file $t | grep user)
         done
       done
