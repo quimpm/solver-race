433e2faa62837bb81746c72800a1f63c6f5f7875
kimetix99 <qpico99@gmail.com>
2020-04-21 16:22:00 +0200

    some order

--- solver/data/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from solver.data.parser import Parser
-from solver.data.literal import Literal
-from solver.data.clause import Clause
-from solver.data.formula import Formula
--- solver/sat/walksat.py
+++
@@ -1,8 +0,0 @@
-from solver.data.parser import Parser
-from .SAT import SAT
-
-
-class WalkSAT(SAT):
-
-    def get_random_interpretation(self):
-        pass
