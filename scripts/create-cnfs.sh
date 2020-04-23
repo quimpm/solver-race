3376a5d0a35c81e27ec56faae0decf80edcfce0c
Oriol <oriolalascercos@gmail.com>
2020-04-23 13:36:14 +0200

    Reformed dir 200

--- scripts/create-cnfs.sh
+++ scripts/create-cnfs.sh
@@ -1,9 +1,9 @@
 
-for i in {40..85}; do
+for i in {20..42}; do
   prove=""
   SAT="SATISFIABLE"
-  a=$(($i*10))
-  ./rnd-cnf-gen.py 200 $i 3 > cnfs/200/exemple-200-$a.cnf
+  a=$((200*$i/10))
+  ./rnd-cnf-gen.py 200 $a 3 > cnfs/200/exemple-200-$a.cnf
   prove=$(./minisat cnfs/200/exemple-200-$a.cnf 2>&1 /dev/null | grep SATISFIABLE)
 done
 
